ca05473a98f4ac04ea3d11f4d0f62f1cc0ecd1c7
Entra21 <entra21@proway.treina>
2020-07-10 19:30:29 -0300

    criado .gitignore

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Você é maior de idade?");
+             
+            var amILegalAge = Console.ReadLine() == "sim";
+            
+            Console.WriteLine(Myboolean);
+            
+        }
+    }
+}
